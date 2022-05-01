1cd9807723abbe7d9e2ef3cc43aaae0fb99db8b5
ras24 <siregarassire@gmail.com>
2022-05-02 00:07:44 +0800

    percabangan-1

--- main.py
+++ main.py
@@ -9,3 +9,15 @@ print('Ibu berkata, "Pergi ke toko"')
 print('Budi manjawab, "Baik, apa yang harus saya lakukan di toko?"')
 print('Ibu menjawab, "Beli satu botol susu, dan jika ada telor beli 6"')
 print("Dan mulai berbelanja")
+
+# Percabangan
+jumlah_botol_susu = 1
+
+if jumlah_botol_susu > 0:
+    print("Budi mengecek harganya, dan ternyata uangnya cukup")
+    print("Budi membeli 1 botol susu")
+else:
+    print("Budi tidak jadi membeli 1 botol susu")
+
+print("Budi pulang ke rumah")
+print("Menyampaikan hasilnya kepada Ibu")
