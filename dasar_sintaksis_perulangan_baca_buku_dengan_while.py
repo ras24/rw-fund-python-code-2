a1012f6805d5239ed42eb69dd6a2b33881328f7d
ras24 <siregarassire@gmail.com>
2022-05-02 13:07:01 +0800

    update-perulangan-while

--- dasar_sintaksis_perulangan_baca_buku_dengan_while.py
+++ dasar_sintaksis_perulangan_baca_buku_dengan_while.py
@@ -1,14 +1,14 @@
 """
 Program perulangan membaca buku dengan while
 """
-jumlah_buku = 1000
+jumlah_buku = 10
 print('Ibu berkata, "Baca semua buku"')
 
-jumlah_buku_yang_sudah_dibaca = 0
-print(f'Jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
+jumlah_buku_yang_sudah_dibaca_dan_dipahami = 0
+print(f'Jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
 
-while jumlah_buku_yang_sudah_dibaca < jumlah_buku:
-    jumlah_buku_yang_sudah_dibaca = jumlah_buku_yang_sudah_dibaca + 1
-    print(f"Baca ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca")
+while jumlah_buku_yang_sudah_dibaca_dan_dipahami < jumlah_buku:
+    jumlah_buku_yang_sudah_dibaca_dan_dipahami = jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1
+    print(f"Baca ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami} sudah dibaca dan dipahami")
 
-print(f'Jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
+print(f'Jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
